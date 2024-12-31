677119245a7cded483797a716ac3b8d35964d1fa
omargade <omargade2208@gmail.com>
2024-12-31 21:13:17 +0530

    recent-expense-modified

--- server/routes/account.js
+++ server/routes/account.js
@@ -16,11 +16,11 @@ const accountInfoSchema = zod.object({
     budget: zod.number()
         .min(0, { message: "budget cannot be less than zero" }),
 
-    date: zod.string().transform((val) => new Date(val)).optional() 
+    date: zod.string().transform((val) => new Date(val)).optional()
 });
 
 function accountInfoInputValidation(req, res, next) {
-    try{
+    try {
         const response = accountInfoSchema.safeParse(req.body);
         if (!response.success) {
             const errors = response.error.errors.map((err) => ({
@@ -31,23 +31,23 @@ function accountInfoInputValidation(req, res, next) {
             return res.status(400).json(errors);
         }
 
-        if(req.body.balance > req.body.income){
+        if (req.body.balance > req.body.income) {
             return res.status(400).json({
-                path : ["balance"],
-                message : "blance cannot exceed income"
+                path: ["balance"],
+                message: "blance cannot exceed income"
             });
         }
-        if(req.body.budget > req.body.balance){
+        if (req.body.budget > req.body.balance) {
             return res.status(400).json({
-                path : ["budget"],
-                message : "budget cannot exceed balance"
+                path: ["budget"],
+                message: "budget cannot exceed balance"
             });
         }
 
         next();
-    }catch(error){
+    } catch (error) {
         console.log(`error during zod ${error}`);
-        return res.status(500).json({message : "internal zod error"});
+        return res.status(500).json({ message: "internal zod error" });
     }
 }
 
@@ -76,44 +76,44 @@ accountRouter.post("/addaccountinfo", authMiddleware, accountInfoInputValidation
 })
 
 
-accountRouter.get("/currentinfo",authMiddleware, async (req, res) => {
-    try{
-        const accountInfo = await Account.findOne({userId : req.userId} , {_id : 0, userId : 0 , __v : 0});
-        if(!accountInfo){
+accountRouter.get("/currentinfo", authMiddleware, async (req, res) => {
+    try {
+        const accountInfo = await Account.findOne({ userId: req.userId }, { _id: 0, userId: 0, __v: 0 });
+        if (!accountInfo) {
             return res.status(404).json({
-                message : "Account information is not added"
+                message: "Account information is not added"
             })
         }
         res.json(accountInfo);
 
-    }catch(error){
+    } catch (error) {
         console.log(`error during fetching account data ${error}`)
         return res.status(500).json({
-            message : "internal server error"
+            message: "internal server error"
         })
     }
-    
+
 })
 
 
 const expenseValidationSchema = zod.object({
-    amount : zod.number().min(0,{message : "please enter the spend amount"}),
-    category : zod.enum(['food', 'subscription', 'shopping', 'rent', 'bill', 'travel', 'other'],{
-        errorMap : () => ({message : "please select valid category"})
+    amount: zod.number().min(0, { message: "please enter the spend amount" }),
+    category: zod.enum(['food', 'subscription', 'shopping', 'rent', 'bill', 'travel', 'other'], {
+        errorMap: () => ({ message: "please select valid category" })
     }),
-    description : zod.string().min(0,{message : "please enter the description"}),
-    spendDate : zod.string().transform((val)=> new Date(val)),
-    createdAt : zod.string().transform((val)=> new Date(val)).optional()
+    description: zod.string().min(0, { message: "please enter the description" }),
+    spendDate: zod.string().transform((val) => new Date(val)),
+    createdAt: zod.string().transform((val) => new Date(val)).optional()
 })
 
-function validateExpenseInput(req,res,next){
+function validateExpenseInput(req, res, next) {
     const response = expenseValidationSchema.safeParse(req.body);
-    if(!response.success){
+    if (!response.success) {
         const errors = response.error.errors.map(err => ({
-            path : err.path,
-            message : err.message
+            path: err.path,
+            message: err.message
         }));
-        
+
         return res.status(400).json(errors);
     }
     next();
@@ -121,11 +121,11 @@ function validateExpenseInput(req,res,next){
 
 const getFirstDayOfNextMonth = (date) => {
     const givenDate = new Date(date);
-    return new Date(givenDate.getFullYear(),givenDate.getMonth()+1,1);
+    return new Date(givenDate.getFullYear(), givenDate.getMonth() + 1, 1);
 }
 const getFirstDayOfMonth = (date) => {
     const givenDate = new Date(date);
-    return new Date(givenDate.getFullYear(),givenDate.getMonth(),1);
+    return new Date(givenDate.getFullYear(), givenDate.getMonth(), 1);
 }
 
 async function storeExpense(req, res, next) {
@@ -138,7 +138,7 @@ async function storeExpense(req, res, next) {
 
         const accountInfo = await Account.findOne(
             { userId },
-            { _id: 1, balance: 1, budget: 1, totalSpend: 1, date : 1 }
+            { _id: 1, balance: 1, budget: 1, totalSpend: 1, date: 1 }
         ).session(session);
 
         if (!accountInfo) {
@@ -153,15 +153,15 @@ async function storeExpense(req, res, next) {
         const firstDayOfAccountMonth = getFirstDayOfMonth(accountInfo.date);
         const spendDateObj = new Date(spendDate);
 
-        console.log(`first date of current month ${firstDayOfAccountMonth}`);
-        console.log(`first date of next month ${firstDayOfNextMonth}`);
-        console.log(`spend date ${spendDateObj}`);
+        // console.log(`first date of current month ${firstDayOfAccountMonth}`);
+        // console.log(`first date of next month ${firstDayOfNextMonth}`);
+        // console.log(`spend date ${spendDateObj}`);
 
-        if(spendDateObj <= firstDayOfAccountMonth || spendDateObj >= firstDayOfNextMonth){
+        if (spendDateObj <= firstDayOfAccountMonth || spendDateObj >= firstDayOfNextMonth) {
             await session.abortTransaction();
             session.endSession();
             return res.status(400).json({
-                message : "please enter the expense for this month only"
+                message: "please enter the expense for this month only"
             })
         }
 
@@ -229,69 +229,76 @@ async function storeExpense(req, res, next) {
     }
 }
 
-accountRouter.post("/addexpense",authMiddleware,validateExpenseInput,storeExpense,(req,res)=>{
-    
+accountRouter.post("/addexpense", authMiddleware, validateExpenseInput, storeExpense, (req, res) => {
+
     const responsePayload = req.responsePayload;
 
     res.json({
-        message : "expense added successfully",
+        message: "expense added successfully",
         responsePayload
     })
 })
 
-accountRouter.get("/recentexpense",authMiddleware,async (req,res)=>{
-   const {category, startdate, enddate} = req.query;
-    try{
+accountRouter.get("/recentexpense", authMiddleware, async (req, res) => {
+    const { category, startdate, enddate } = req.query;
+    try {
         const hasFilter = category || startdate || enddate;
-        if(hasFilter){
-            const query = {userId : req.userId};
+        if (hasFilter) {
+            const query = { userId: req.userId };
 
-            if(category){
+            if (category) {
                 query.category = category;
             }
 
-            if(startdate || enddate){
+            if (startdate || enddate) {
                 query.spendDate = {}
-                if(startdate){
-                    query.spendDate.$gte = new Date(startdate);
+                console.log(startdate);
+                if (startdate) {
+                    const formattedStartDate = startdate.split('-').map(part => part.padStart(2, '0')).join('-'); 
+                    const startDateObj = new Date(`${formattedStartDate}T00:00:00.000Z`);
+                    console.log(startDateObj);
+                    query.spendDate.$gte = startDateObj;
                 }
-                if(enddate){
-                    query.spendDate.$lte = new Date(enddate);
+                if (enddate) {
+                    const formattedEndDate = enddate.split('-').map(part => part.padStart(2,'0')).join('-');
+                    const endDateObj = new Date(`${formattedEndDate}T00:00:00.000Z`); 
+                    console.log(endDateObj);
+                    query.spendDate.$lte = endDateObj;
                 }
             }
+            console.log(query);
+            const filteredResponse = await Expense.find(query, { _id: 0, userId: 0, accountId: 0, __v: 0 })
+                .sort({ spendDate: -1 });
 
-            const filteredResponse = await Expense.find(query , {_id : 0, userId : 0, accountId : 0, __v : 0})
-                .sort({spendDate : -1});
-
-            if(filteredResponse.length === 0){
+            if (filteredResponse.length === 0) {
                 return res.status(404).json({
-                    message : "No transactions found"
+                    message: "No transactions found"
                 })
             }
 
             return res.json(
                 {
                     filteredResponse,
-                    total : filteredResponse.length
+                    total: filteredResponse.length
                 });
         }
 
-        const recentexpense = await Expense.find({userId : req.userId} , {_id : 0, userId : 0, accountId : 0, __v: 0})
-                .sort({spendDate : -1})
-                .limit(10)
-        
-        if(recentexpense.length === 0){
+        const recentexpense = await Expense.find({ userId: req.userId }, { _id: 0, userId: 0, accountId: 0, __v: 0 })
+            .sort({ spendDate: -1 })
+            .limit(10)
+
+        if (recentexpense.length === 0) {
             return res.status(404).json({
-                message : "No transactions found"
+                message: "No transactions found"
             })
         }
 
         res.json({
             recentexpense,
-            total : recentexpense.length
+            total: recentexpense.length
         })
 
-    } catch(error){
+    } catch (error) {
         console.error(`Error while getting the recent transactions: ${error}`);
         return res.status(500).json({ message: "Internal server error" });
     }
