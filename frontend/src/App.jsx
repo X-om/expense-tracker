68afd2d5a78146fb2dc6728bc74f7868eb5aca3f
omargade <omargade2208@gmail.com>
2025-01-06 01:36:43 +0530

    nextUI-added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
-
+import { Button } from "@nextui-org/react"
 function App() {
 
   return (
-    <div>
-        <h1>Hello World</h1>
+    <div className="flex bg-black justify-center h-screen">
+          <div className="flex flex-col justify-center">
+            <Button color="primary" isLoading  size="lg" radius="full">Downloading</Button>
+          </div>
     </div>
   )
 }
